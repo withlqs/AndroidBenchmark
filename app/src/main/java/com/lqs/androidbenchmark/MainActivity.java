dab266831e12a99154ed8a7532e6b6f840ae6178
withlqs <withlqs@gmail.com>
2016-05-28 23:43:56 +0800

    wait to collect data

--- app/src/main/java/com/lqs/androidbenchmark/MainActivity.java
+++ app/src/main/java/com/lqs/androidbenchmark/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,9 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     List<BenchmarkTask> taskList = new ArrayList<>();
     List<Long> resultList = new ArrayList<>();
-    private TextView textView;
-    private EditText periodEditText;
-    private EditText threadNumberEditText;
+    int[] idList = {
+            R.id.single_int,
+            R.id.multi_int,
+            R.id.single_float,
+            R.id.multi_float,
+
+    };
     private int nowTaskPtr = 0;
 
     @Override
@@ -26,84 +29,91 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.testView);
-        textView.setText(BenchmarkJNI.getDemoString(1));
+//        textView = (TextView) findViewById(R.id.testView);
+//        textView.setText(BenchmarkJNI.getDemoString(1));
+//
+//        periodEditText = (EditText) findViewById(R.id.periodEditText);
+//        periodEditText.setText("100000000");
+//        threadNumberEditText = (EditText) findViewById(R.id.threadNumberEditText);
+//        threadNumberEditText.setText("1");
+//
+//        Button button1 = (Button) findViewById(R.id.button1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int period = new Integer(periodEditText.getText().toString());
+//                int threadNumber = new Integer(threadNumberEditText.getText().toString());
+//                ResultProcessor resultProcessor = new ResultProcessor();
+//                BenchmarkTask benchmarkTask = new BenchmarkTask(
+//                        getProgressDialog(),
+//                        resultProcessor,
+//                        BenchmarkMethod.floatBenchmarkMethod,
+//                        threadNumber, period
+//                );
+//                benchmarkTask.execute();
+//            }
+//        });
+//
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int period = new Integer(periodEditText.getText().toString());
+//                int threadNumber = new Integer(threadNumberEditText.getText().toString());
+//                ResultProcessor resultProcessor = new ResultProcessor();
+//                BenchmarkTask benchmarkTask = new BenchmarkTask(
+//                        getProgressDialog(),
+//                        resultProcessor,
+//                        BenchmarkMethod.integerBenchmarkMethod,
+//                        threadNumber,
+//                        period
+//                );
+//                benchmarkTask.execute();
+//            }
+//        });
 
-        periodEditText = (EditText) findViewById(R.id.periodEditText);
-        periodEditText.setText("100000000");
-        threadNumberEditText = (EditText) findViewById(R.id.threadNumberEditText);
-        threadNumberEditText.setText("1");
 
-        Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int period = new Integer(periodEditText.getText().toString());
-                int threadNumber = new Integer(threadNumberEditText.getText().toString());
-                ResultProcessor resultProcessor = new ResultProcessor();
-                BenchmarkTask benchmarkTask = new BenchmarkTask(
-                        getProgressDialog(),
-                        resultProcessor,
-                        BenchmarkMethod.floatBenchmarkMethod,
-                        threadNumber, period
-                );
-                benchmarkTask.execute();
-            }
-        });
-
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int period = new Integer(periodEditText.getText().toString());
-                int threadNumber = new Integer(threadNumberEditText.getText().toString());
-                ResultProcessor resultProcessor = new ResultProcessor();
-                BenchmarkTask benchmarkTask = new BenchmarkTask(
-                        getProgressDialog(),
-                        resultProcessor,
+                nowTaskPtr = 0;
+                resultList.clear();
+                taskList.clear();
+                taskList.add(new BenchmarkTask(
+                        getProgressDialog("正在进行CPU单线程整数性能测试"),
+                        new ResultProcessor(),
                         BenchmarkMethod.integerBenchmarkMethod,
-                        threadNumber,
-                        period
-                );
-                benchmarkTask.execute();
+                        1,
+                        100000000
+                ));
+
+                taskList.add(new BenchmarkTask(
+                        getProgressDialog("正在进行CPU多线程整数性能测试"),
+                        new ResultProcessor(),
+                        BenchmarkMethod.integerBenchmarkMethod,
+                        8,
+                        3000000
+                ));
+
+                taskList.add(new BenchmarkTask(
+                        getProgressDialog("正在进行CPU单线程浮点数性能测试"),
+                        new ResultProcessor(),
+                        BenchmarkMethod.floatBenchmarkMethod,
+                        1,
+                        100000000
+                ));
+
+                taskList.add(new BenchmarkTask(
+                        getProgressDialog("正在进行CPU多线程浮点数性能测试"),
+                        new ResultProcessor(),
+                        BenchmarkMethod.floatBenchmarkMethod,
+                        8,
+                        3000000
+                ));
+                taskList.get(0).execute();
             }
         });
-
-
-        taskList.add(new BenchmarkTask(
-                getProgressDialog("正在进行CPU单线程整数运算性能测试..."),
-                new ResultProcessor(),
-                BenchmarkMethod.integerBenchmarkMethod,
-                1,
-                100000000
-        ));
-
-        taskList.add(new BenchmarkTask(
-                getProgressDialog("正在进行CPU单线程浮点数运算性能测试..."),
-                new ResultProcessor(),
-                BenchmarkMethod.floatBenchmarkMethod,
-                1,
-                100000000
-        ));
-
-        taskList.add(new BenchmarkTask(
-                getProgressDialog("正在进行CPU多线程整数运算性能测试..."),
-                new ResultProcessor(),
-                BenchmarkMethod.integerBenchmarkMethod,
-                8,
-                3000000
-        ));
-
-        taskList.add(new BenchmarkTask(
-                getProgressDialog("正在进行CPU多线程浮点数运算性能测试..."),
-                new ResultProcessor(),
-                BenchmarkMethod.floatBenchmarkMethod,
-                8,
-                3000000
-        ));
-
-
-        taskList.get(0).execute();
     }
 
     private ProgressDialog getProgressDialog() {
@@ -120,25 +130,28 @@ public class MainActivity extends AppCompatActivity {
 
     public class ResultProcessor {
         private long calculateSingle(long time) {
-            return 22679757 / time;
+            return 22094550 / time;
         }
 
         private long calculateMulti(long time) {
-            return 191489600 / time;
+            return 143617200 / time;
         }
 
         public void sendResult(long time) {
 
-            if (nowTaskPtr < 2) {
+            if (nowTaskPtr % 2 == 0) {
                 resultList.add(calculateSingle(time));
             } else {
                 resultList.add(calculateMulti(time));
             }
-            textView.setText(textView.getText().toString() + "\n" + resultList.get(nowTaskPtr));
+
+            TextView textView = (TextView) findViewById(idList[nowTaskPtr]);
+            textView.setText("" + resultList.get(nowTaskPtr));
 
             ++nowTaskPtr;
             if (nowTaskPtr < taskList.size()) {
                 taskList.get(nowTaskPtr).execute();
+            } else {
             }
         }
     }
